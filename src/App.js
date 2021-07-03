e2ec60a39dabdbea541066a2d074821bf26c7a31
IchwanSetyawan <ichwansetyawan2015@gmail.com>
2021-07-03 13:44:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import List from './components/List';
+
 
 function App() {
+  const[data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(res =>res.json())
+  .then(dataApi=>{
+    setData(dataApi)
+  })
+  .catch(err=> setError('Error ketika mengambil data dari third party API!'))  
+  .finally(()=> setLoading(false))
+
+
+},[]) 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+        <h1>Fetc API in React</h1>
+        {
+          loading === true ? <h2>Loading ...</h2> : 
+          <ul>
+            {data.map(item=>
+              <List 
+                key={item.id}
+                title={item.title} />            
+            )}
+          </ul>
+      
+        }
+      
+      {/* dijalankan ketika ada error yang muncul jika tidak ada maka tidak akan ditampilkan */}
+      {error && <h2 style={{color: 'red'}}>{error}</h2>}
+        
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List = (props) => {
+    return (
+        <div>
+            <li>{props.title}</li>
+        </div>
+    );
+};
+
+export default List;
